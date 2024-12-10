a536ffbb76bf22b0183daa0a500e45cb73f09439
selamalka <selamalka@gmail.com>
2024-12-10 17:43:15 +0200

    Turn switches between players

--- Assets/Scripts/HumanPlayerController.cs
+++ Assets/Scripts/HumanPlayerController.cs
@@ -1,27 +1,8 @@
 
 public class HumanPlayerController : BasePlayerController
 {
-    private void OnDisable()
-    {
-        GameManager.GetConnectGameGrid().ColumnClicked -= HandleColumnClicked;
-    }
-
-    private void Start()
-    {
-        GameManager.GetConnectGameGrid().ColumnClicked += HandleColumnClicked;
-    }
-
-    private void HandleColumnClicked(int column)
-    {
-        if (IsMyTurn())
-        {
-            MakeMove(column); 
-        }
-    }
-
     public override void MakeMove(int column)
     {
-        print($"{PlayerColor} made a move");
         GameManager.OnTurnEnded?.Invoke(PlayerColor);
     }
 }
