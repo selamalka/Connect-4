98ade44e1f97ece8816afeaae34ec6e0e56e6b4a
selamalka <selamalka@gmail.com>
2024-12-10 19:56:19 +0200

    Managed to use StoppedFalling event in GameManager

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,25 +1,23 @@
 using UnityEngine;
 using MoonActive.Connect4;
+using System;
 
 public class Cell : MonoBehaviour
 {
     [field: SerializeField] public int Row { get; private set; }
     [field: SerializeField] public int Column { get; private set; }
     [field: SerializeField] public PlayerColor PlayerInCell { get; private set; }
-    private Disk diskInCell;
 
     public void SetRow(int row) { Row = row; }
     public void SetColumn(int column) { Column = column; }
-    public void SetState(PlayerColor player) { PlayerInCell = player; }
+    public void SetPlayerInCell(PlayerColor player) { PlayerInCell = player; }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<Disk>())
-        {            
-            diskInCell = collision.gameObject.GetComponent<Disk>();
-
-            PlayerColor color = diskInCell.GetComponent<DiskIdentifier>().Color;
-            PlayerInCell = color;
+        if (collision.gameObject.GetComponent<Disk>() != null)
+        {
+            PlayerColor diskColor = collision.gameObject.GetComponent<DiskIdentifier>().Color;
+            SetPlayerInCell(diskColor);
         }
     }
 }
