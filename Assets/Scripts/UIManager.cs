ee4c945ae8800e5e39bffd3b79272012d7a74a7d
selamalka <selamalka@gmail.com>
2024-12-13 14:08:18 +0200

    Added some more juice to the UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,6 @@
+using DG.Tweening;
 using System;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +10,12 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject mainPanel;
     [SerializeField] private GameObject board;
     [SerializeField] private GameObject closeButton;
+    [SerializeField] private GameObject choosePlayerPanel;
+    [SerializeField] private GameObject header;
+
+    [SerializeField] private Image playerVsPlayerButton;
+    [SerializeField] private Image playerVsComputerButton;
+    [SerializeField] private Color chosenModeColor;
 
     [SerializeField] private GameObject announcementPanel;
     [SerializeField] private Text announcementText;
@@ -16,6 +24,8 @@ public class UIManager : MonoBehaviour
     public static event Action<GameMode> OnConfirmPressed;
     public static event Action OnRestartPressed;
     public static Action<string> OnAnnouncement;
+
+    private int delayTimeAfterPress = 250;
 
     private GameManager gameManager;
 
@@ -41,15 +51,44 @@ public class UIManager : MonoBehaviour
         announcementText.text = announcement;
     }
 
-    private void Start()
+    private async void Start()
     {       
         if (gameManager != null) closeButton.SetActive(gameManager.IsGameActive);
         announcementPanel.SetActive(false);
+        HandleModeButtonColor();
+        choosePlayerPanel.transform.localScale = Vector3.zero;
+        choosePlayerPanel.SetActive(true);
+        header.transform.localScale = Vector3.zero;
+        header.SetActive(true);
+        await Task.Delay(200);
+        header.transform.DOScale(1f, 0.5f).SetEase(Ease.OutBack);
+        await Task.Delay(100);
+        choosePlayerPanel.transform.DOScale(1f, 0.5f).SetEase(Ease.OutBack);
+    }
+
+    private void HandleModeButtonColor()
+    {
+        switch (gameManager.GameMode)
+        {
+            case GameMode.PlayerVsPlayer:                
+                playerVsPlayerButton.DOColor(chosenModeColor, 0.2f).SetEase(Ease.OutQuad);
+                playerVsComputerButton.DOColor(Color.white, 0.2f).SetEase(Ease.OutQuad);
+                break;
+
+            case GameMode.PlayerVsComputer:
+                playerVsComputerButton.DOColor(chosenModeColor, 0.2f).SetEase(Ease.OutQuad);
+                playerVsPlayerButton.DOColor(Color.white, 0.2f).SetEase(Ease.OutQuad);
+                break;
+
+            default:
+                break;
+        }
     }
 
     // Unity event
-    public void MenuButton()
+    public async void MenuButton()
     {
+        await Task.Delay(delayTimeAfterPress);
         announcementPanel.SetActive(false);
         mainPanel.SetActive(true);
         closeButton.SetActive(gameManager.IsGameActive);
@@ -57,8 +96,9 @@ public class UIManager : MonoBehaviour
     }
 
     // Unity event
-    public void ConfirmButton()
+    public async void ConfirmButton()
     {
+        await Task.Delay(delayTimeAfterPress);
         mainPanel.SetActive(false);
         board.SetActive(true);
         OnConfirmPressed?.Invoke(gameManager.GameMode);
@@ -68,25 +108,29 @@ public class UIManager : MonoBehaviour
     public void PlayerVsPlayerButton()
     {
         OnSelectGameMode?.Invoke(GameMode.PlayerVsPlayer);
+        HandleModeButtonColor();
     }
 
     // Unity event
     public void PlayerVsComputerButton()
     {
         OnSelectGameMode?.Invoke(GameMode.PlayerVsComputer);
+        HandleModeButtonColor();
     }
 
     // Unity event
-    public void RestartButton()
+    public async void RestartButton()
     {
+        await Task.Delay(delayTimeAfterPress);
         announcementPanel.SetActive(false);
         board.SetActive(true);
         OnRestartPressed?.Invoke();
     }
 
     // Unity event
-    public void CloseButton()
+    public async void CloseButton()
     {
+        await Task.Delay(delayTimeAfterPress);
         mainPanel.SetActive(false);
     }
 }
